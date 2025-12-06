a9f0317135721ef54da041bcf70520fc25c8abf2
jiminu <jmu417@gmail.com>
2025-12-06 15:10:20 +0900

    first commit

---
+++ src/main/java/com/factory/tycoon/TycoonApplication.java
@@ -0,0 +1,13 @@
+package com.factory.tycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TycoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TycoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/tycoon/TycoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.tycoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TycoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
