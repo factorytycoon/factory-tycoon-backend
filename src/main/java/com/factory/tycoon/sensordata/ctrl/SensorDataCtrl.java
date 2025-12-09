2be981d059246d0f4bcad553b2b4ba6c2ef0682f
jiminu <jmu417@gmail.com>
2025-12-09 18:37:05 +0900

    chors: delete unnecessary import

--- src/main/java/com/factory/tycoon/sensordata/ctrl/SensorDataCtrl.java
+++ src/main/java/com/factory/tycoon/sensordata/ctrl/SensorDataCtrl.java
@@ -7,11 +7,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Tag(name = "SensorData", description = "센서 데이터 관리 API")
