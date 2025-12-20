d971b3b09c0d9ef292eaec47c12f21404a2f4c00
jiminu <jmu417@gmail.com>
2025-12-20 19:39:58 +0900

    fix: disable credential

--- src/main/java/com/factory/tycoon/config/WebMvcConfig.java
+++ src/main/java/com/factory/tycoon/config/WebMvcConfig.java
@@ -25,13 +25,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:3000",
-                        "http://localhost:3001"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3600);
     }
 
