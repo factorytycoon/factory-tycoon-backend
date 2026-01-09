78af1cc830c9d7f3a4c4c06b3c0f1157e1570484
j2hxxx <77jihyun77@naver.com>
2026-01-09 09:39:41 +0900

    feat : 특정 factory의 Workorder를 조회하는 API 추가

--- src/main/java/com/factory/tycoon/workorder/repository/WorkOrderRepository.java
+++ src/main/java/com/factory/tycoon/workorder/repository/WorkOrderRepository.java
@@ -3,6 +3,8 @@ package com.factory.tycoon.workorder.repository;
 import com.factory.tycoon.workorder.domain.entity.WorkOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,7 @@ import java.util.List;
 public interface WorkOrderRepository extends JpaRepository<WorkOrderEntity, Long> {
     List<WorkOrderEntity> findByOrderId(Long orderId);
     List<WorkOrderEntity> findByEquipmentId(Long equipmentId);
+
+    @Query("SELECT w FROM WorkOrderEntity w JOIN OrderEntity o ON w.orderId = o.orderId WHERE o.factory.factoryId = :factoryId")
+    List<WorkOrderEntity> findByFactoryId(@Param("factoryId") Long factoryId);
 }
