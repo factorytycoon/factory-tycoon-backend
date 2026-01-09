02625a210e3e845b2c43fbb9413beb52a4bee5e4
junhee95 <jhjang.vdrc@gmail.com>
2026-01-09 09:28:28 +0900

    pull main and fix data-analysis

--- src/main/java/com/factory/tycoon/alarm/repository/AlarmRepository.java
+++ src/main/java/com/factory/tycoon/alarm/repository/AlarmRepository.java
@@ -4,21 +4,14 @@ import com.factory.tycoon.alarm.domain.entity.AlarmEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<AlarmEntity, Long> {
-<<<<<<< HEAD
-    List<AlarmEntity> findBySensor_SensorId(Long sensorId);
-    List<AlarmEntity> findByLevel(AlarmLevel level);
-    List<AlarmEntity> findByStatus(Boolean status);
-    List<AlarmEntity> findBySensor_SensorIdAndLevel(Long sensorId, AlarmLevel level);
-    List<AlarmEntity> findBySensor_SensorIdAndStatus(Long sensorId, Boolean status);
-    List<AlarmEntity> findAllByCreatedAtBetween(LocalDateTime startOfDay,LocalDateTime endOfDay);
-=======
+
     List<AlarmEntity> findByStatus(String status);
     List<AlarmEntity> findByEquipmentId(Long equipmentId);
     List<AlarmEntity> findByEquipmentIdAndStatus(Long equipmentId, String status);
->>>>>>> main
+    List<AlarmEntity> findAllByCreatedAtBetween(java.time.LocalDateTime startOfDay, java.time.LocalDateTime endOfDay);
+
 }
