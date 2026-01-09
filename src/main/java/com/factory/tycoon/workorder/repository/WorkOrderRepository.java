3572dbc69d0f90ebb9c90052fb80b72e19d207ae
j2hxxx <77jihyun77@naver.com>
2026-01-09 13:23:27 +0900

    fix : API join 방법 수정

--- src/main/java/com/factory/tycoon/workorder/repository/WorkOrderRepository.java
+++ src/main/java/com/factory/tycoon/workorder/repository/WorkOrderRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface WorkOrderRepository extends JpaRepository<WorkOrderEntity, Long> {
-        @Query("SELECT w FROM WorkOrderEntity w JOIN OrderEntity o WHERE w.orderId = o.orderId AND o.factory.factoryId = :factoryId AND w.status = true")
+        @Query("SELECT w FROM WorkOrderEntity w JOIN OrderEntity o ON w.orderId = o.orderId WHERE o.factory.factoryId = :factoryId AND w.status = true")
         List<WorkOrderEntity> findByFactoryIdAndStatusTrue(@Param("factoryId") Long factoryId);
     List<WorkOrderEntity> findByOrderId(Long orderId);
     List<WorkOrderEntity> findByEquipmentId(Long equipmentId);
